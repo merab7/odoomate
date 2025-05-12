de1dd52796e05e04c5596a023c2b1c2ee6e802e4
merab7 <merabtodua7@gmail.com>
2025-05-12 15:55:40 +0400

    initial commit form odoo matew

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Hospital Management System",
+    "author":"Merab Todua",
+    "version": "17.0.1.1",
+
+    "data":[
+      'security/ir.model.access.csv',
+      'views/patient_views.xml',
+      'views/menu.xml'
+
+    ],
+
+    "license": "LGPL-3",
+}
